af1087d8252100512186fe1b2376f8aa7be5485a
yangyang zhong <35210666+hdygxsj@users.noreply.github.com>
2024-10-29 23:04:15 +0800

    [Feature] 支持创建K8sClient

--- celeborn-kubernetes-operator-server/src/main/java/io/hdygxsj/celeborn/kubernetes/operator/server/configuration/KubernetesClientConfiguration.java
+++ celeborn-kubernetes-operator-server/src/main/java/io/hdygxsj/celeborn/kubernetes/operator/server/configuration/KubernetesClientConfiguration.java
@@ -1,17 +1,63 @@
 package io.hdygxsj.celeborn.kubernetes.operator.server.configuration;
 
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+@Slf4j
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties("kubernetes.client")
 public class KubernetesClientConfiguration {
+
+    private String type;
+
+    private String path;
+
+    private String ca;
+
+    private String token;
+
+    private String masterUrl;
 
     @Bean
     public KubernetesClient kubernetesClient() {
-        return new KubernetesClientBuilder().build();
+        Config config = null;
+        switch (type) {
+            case "kubeconfig":
+                log.info("Creating Kubernetes client with type: {}", type);
+                try {
+                    config = Config.fromKubeconfig(Files.readString(Paths.get(path), java.nio.charset.StandardCharsets.UTF_8));
+                } catch (Exception e) {
+                    log.error("Error loading kubeconfig: {}", e.getMessage());
+                }
+                break;
+            case "token":
+                log.info("Creating Kubernetes client use masterUrl,token,ca");
+                config = new ConfigBuilder()
+                        .withMasterUrl(masterUrl)
+                        .withCaCertData(ca)
+                        .withOauthToken(token)
+                        .build();
+                break;
+            default:
+                log.info("Creating Kubernetes client use service account");
+                break;
+
+        }
+        return new KubernetesClientBuilder().withConfig(config).build();
     }
 
 }
